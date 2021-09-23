d17093e18c4751930d158a19a8833b657bfbb946
Jacob Hopkins <smikahopkins@gmail.com>
2021-09-22 20:30:40 -0500

    Update HardwareERDatabaseCreation.sql

--- HardwareERDatabaseCreation.sql
+++ HardwareERDatabaseCreation.sql
@@ -1,8 +1,33 @@
+#CREATETABLE STATEMENTS
+
+CREATE TABLE Items (
+    itemID int NOT NULL PRIMARY KEY,
+    name varchar(255),
+    description varchar(255)
+);
+
 CREATE TABLE Services (
-    serviceID int,
+    serviceID int NOT NULL PRIMARY KEY,
     name varchar(255),
     description varchar(255)
 ); 
+
+CREATE TABLE ItemInServiceUse (
+    itemID int FOREIGN KEY REFERENCES Items(itemID),
+    serviceID int FOREIGN KEY REFERENCES Services(serviceID),
+);
+
+CREATE TABLE People ();
+
+CREATE TABLE Supplier ();
+
+CREATE TABLE Employee ();
+
+CREATE TABLE Customer ();
+
+CREATE TABLE Items ();
+
+#INSERT STATEMENTS
 
 INSERT INTO Services (100,"duplicate","type 1 key");
 INSERT INTO Services (101,"duplicate","type 2 key");
