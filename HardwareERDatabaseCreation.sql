246aeefad44d963b439cd88d4b08aff9a0efa292
Jacob Hopkins <smikahopkins@gmail.com>
2021-09-22 20:21:26 -0500

    Create HardwareERDatabaseCreation.sql

---
+++ HardwareERDatabaseCreation.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Services (
+    serviceID int,
+    name varchar(255),
+    description varchar(255)
+); 
+
+INSERT INTO Services (100,"duplicate","type 1 key");
+INSERT INTO Services (101,"duplicate","type 2 key");
+INSERT INTO Services (102,"repair","re-glaze window");
+INSERT INTO Services (103,"rent","seed spreader");
