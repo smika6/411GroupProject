21d1e00db222806300efd58ccf20041f50f71116
Jacob Hopkins <smikahopkins@gmail.com>
2021-09-27 21:55:34 -0500

    Create showAllTables.sql

---
+++ final/showAllTables.sql
@@ -0,0 +1,29 @@
+SELECT * FROM Address;
+
+SELECT * FROM PersonAddress;
+
+SELECT * FROM SupplierLocation;
+
+SELECT * FROM Person;
+
+SELECT * FROM Employee;
+
+SELECT * FROM Customer;
+
+SELECT * FROM Supplier;
+
+SELECT * FROM Items;
+
+SELECT * FROM Hardware;
+
+SELECT * FROM Services;
+
+SELECT * FROM HardwareInServiceUse;
+
+SELECT * FROM CustomerPurchases;
+
+SELECT * FROM Deliveries;
+
+SELECT * FROM PurchaseFromSupplier;
+
+SELECT * FROM EmployeePerformService;
