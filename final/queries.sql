56102a1d4c8a8654a2c7c1c013d15eb7aaf437ff
Patrick <55414763+pbeech277@users.noreply.github.com>
2021-09-28 17:57:38 -0500

    Update queries.sql

--- final/queries.sql
+++ final/queries.sql
@@ -4,3 +4,18 @@ FROM ADDRESS a, CUSTOMER c, EMPLOYEE e
 WHERE
 a.astreetID = c.customerID AND
 a.astreetID = e.employeeID;
+
+
+/*problem b) */
+SELECT i.itemID, i.iname, i.idescription, a.astreet, a.acity, a.astate
+FROM ITEMS i, ADDRESS a, DELIVERIES d
+WHERE d.streetAddress NOT IN (SELECT a.astreet
+                            FROM ADDRESS a, CUSTOMER c
+                            WHERE a.astreetID = c.customerID ) 
+AND d.streetAddress NOT IN (SELECT a.astreet
+                        FROM ADDRESS a, SUPPLIER s
+                        WHERE a.astreetID = s.supplierID )
+AND i.itemID = d.itemID
+
+AND d.streetAddress = a.astreet;
+
