f2764f11c24ea9a6a8321e5778c93687539528e5
hughesrose <75055157+hughesrose@users.noreply.github.com>
2021-11-07 21:46:52 -0600

    Update procedures_queries.sql

--- procedures_queries.sql
+++ procedures_queries.sql
@@ -87,3 +87,31 @@ END;
 EXECUTE trackAuthorByID (122);
 */
 
+--Rose-------------------------------
+--stored procedure 4
+--displays the user who most recently accessed content
+
+CREATE OR REPLACE PROCEDURE MOST_RECENT AS
+id INTEGER;
+name VARCHAR(50);
+title VARCHAR(150);
+time DATE;
+BEGIN
+SELECT* into id, name, title, time
+FROM(
+SELECT U.userid, P.name, PB.title, PA.view_date 
+FROM USERS U, PERSON P, PUBLICATION PB, PUBACCESSES PA
+WHERE U.userid = P.pid AND
+      PA.pid = PB.pid AND
+      U.userid = PA.userid
+ORDER BY PA.view_date DESC)
+WHERE rownum <= 1;
+ dbms_output.put_line('ID'||'     NAME'||'             TITLE'||'                           DATE');
+ dbms_output.put_line(id||'   '||name||'  '||title||'   '||time);
+END;
+
+
+EXECUTE MOST_RECENT;
+
+--Justification: It makes sure all the ids match up together and then sorts the dates in decending order to get the most recent
+--date on the top then outputs the id, name, title, and date.
